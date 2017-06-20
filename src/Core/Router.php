2f78761a33351709601ab0d955981452dd024c29
ivz-dev <zakutynsky@yahoo.com>
2017-06-20 16:59:29 +0300

    add webhook event

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -34,6 +34,24 @@ class Router
         $this->klein->respond('GET', INDEX_PATH . 'api/' . $this->packageName . '/?', function(){
             include_once dirname(__DIR__) . '/metadata/metadata.json';
         });
+
+
+        $this->klein->respond('POST', INDEX_PATH.'api/Shopify/webhookEvent', function ($request, $response){
+            $requestBody = file_get_contents('php://input');
+            $requestBody = $this->normalizeJson($requestBody);
+            $requestBody = str_replace('\"', '"', $requestBody);
+            $requestBody = json_decode($requestBody, true);
+
+            $reply = [
+                "http_resp" => '',
+                "client_msg" => $requestBody['body'],
+                "params" => $requestBody['params']
+            ];
+            echo json_encode($reply);
+            exit(200);
+        });
+
+
         // Set all routes
         foreach($this->blocks as $blockSettings){
             $this->setRoute($blockSettings);
